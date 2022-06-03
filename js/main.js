f4918782553e28cb5d2804412d7d2fe0d5171797
TrinhAnhTuyen <106819928+TrinhAnhTuyen@users.noreply.github.com>
2022-06-04 00:28:04 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function firstQuestion() {
 
 // switch button position
 function switchButton() {
-    var audio = new Audio('sound/duck.mp3');
+    var audio = new Audio('sound/Swish1.mp3');
     audio.play();
     var leftNo = $('#no').css("left");
     var topNO = $('#no').css("top");
